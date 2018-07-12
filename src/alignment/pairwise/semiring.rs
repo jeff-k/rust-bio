20ecfdf2233a619f697dc0cf0533e2992400bf47
jeff-k <jeff@x-0.ca>
2018-07-11 17:29:06 -0700

    drop in Needleman-Wunsch from partial-order-aligner branch

--- src/alignment/pairwise/semiring.rs
+++ src/alignment/pairwise/semiring.rs
@@ -1,6 +1,8 @@
 use std::ops::{Add, Mul};
-use std::cmp::min;
+use std::cmp::{max, Ordering, min};
 use std::fmt::Debug;
+
+use utils::TextSlice;
 
 extern crate geo;
 use geo::{Polygon, Point};
@@ -62,10 +64,6 @@ fn union(a: Polygon<f64>, b: Polygon<f64>) {
     Polygon::new(vec![], vec![]);
 }
 
-fn minkowski(a: Polygon<f64>, b: Polygon<f64>) {
-    // Minkowski sum (dilation)
-}
-
 impl Add for Polygon<f64> {
     type Output = Polygon<f64>;
     fn add(self, other: Polygon<f64>) -> Polygon<f64> {
@@ -95,7 +93,6 @@ enum Op<C: Eq> {
     M(C, C), // match or substitution
     I, // insertion
     D, // deletion
-    Dx, Ix, // gap open
 }
 
 fn levenshtein(op: Op<u8>) -> Tropical {
@@ -127,19 +124,73 @@ fn polyprop() -> Polytope {
 }
 
 fn align<T: Semiring + Copy + Clone + Debug>
-                    (seq1: &[i32], seq2: &[i32], score: fn(Op<i32>) -> T) {
+                    seq1: TextSlice, seq2: TextSlice, score: fn(Op<i32>) -> T {
     let mut row: Vec<Vec<T>> = vec![vec![T::identity()];seq2.len()];
     row[0][0] = score(Op::M(seq1[0], seq2[0]));
+    let (m, n) = (seq1.len(), seq2.len());
 
-    // defining the boundary condition and recursive structure, which could
-    // be further generalized with algebraic dynamic proramming
-    for j in 1..seq2.len() {
-        for i in 1..seq1.len() {
-            row[i][j]
-                = (score(Op::M(seq1[i], seq2[j])) * row[i-1][j-1])
-                + (score(Op::I) * row[i-1][j])
-                + (score(Op::D) * row[j][i-1]);
+    // initialize matrix
+    let mut traceback: Vec<Vec<Cell>> =
+            vec![vec![T::new(); n + 1]; m + 1];
+    let mut ops: Vec<Op> = vec![];
+
+    for i in 1..(m + 1) {
+        traceback[i][0] = Cell { score: -1 * i as i32, op: Op::Del(None) };
+    }
+    for j in 1..(n + 1) {
+        traceback[0][j] = Cell { score: -1 * j as i32, op: Op::Ins(None) };
+    }
+
+    traceback[0][0] = Cell { score: 0, op: Op::Match(None) };
+
+    // store the last visited node in topological order so that
+    // we can index into the end of the alignment
+    for (i_p, r) in seq1.iter().enumerate() {
+        let i = i_p + 1;
+        // query base and index (traceback matrix rows)
+        for (j_p, q) in seq2.iter().enumerate() {
+            let j = j_p + 1;
+
+            // match and deletion scores for first reference base
+            let (mat, del) = if prevs.len() == 0 {
+                (Cell { score: traceback[0][j - 1].score + (self.scoring)(*r, *q),
+                        op: Op::Match(None) },
+                 Cell { score: traceback[0][j].score - 1i32, op: Op::D })
+            } else {
+                let mut mat_max = Cell { score: T::Inf, op: Op::M };
+                let mut del_max = Cell { score: T::Inf, op: Op::D };
+                for prev_n in 0..prevs.len() {
+                    let i_p: usize = prevs[prev_n].index() + 1;
+                    mat_max = max(mat_max,
+                        Cell { score: traceback[i_p][j - 1].score + (self.scoring)(r, *q),
+                                op: Op::Match(Some((i_p - 1, i - 1)))});
+                    del_max = max(del_max,
+                        Cell { score: traceback[i_p][j].score - 1i32,
+                               op: Op::Del(Some((i_p - 1, i)))});
+                }
+                (mat_max, del_max)
+            };
+            let score = max(mat, max(del, Cell { score: traceback[i][j - 1].score - 1i32, 
+                                                 op: Op::Ins(Some(i - 1)) }));
+            traceback[i][j] = score;
         }
     }
-    row[seq1.len()][row2.len()];
+    
+    // Now backtrack through the matrix to construct the optimal path
+    while i > 0 && j > 0 {
+        // push operation and edge corresponding to (one of the) optimal
+        // routes
+        //println!("\t{}, {} => {}", i, j, traceback[i][j].score);
+        ops.push(traceback[i][j].op.clone());
+        match traceback[i][j].op {
+            Op::M => { },
+            Op::D => { },
+            Op::I => { },
+        }
+    }
+
+    ops.reverse();
+
+    traceback[m][n].score
 }
+
